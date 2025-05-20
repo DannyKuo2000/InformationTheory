365d7966e252c6c319c5f318955db20e18da0a39
DannyKuo2000 <dannykuo2000@gmail.com>
2025-05-20 19:34:11 +0800

    fixed bug

--- InformationTheoryHW4_AB_algorithm.cpp
+++ InformationTheoryHW4_AB_algorithm.cpp
@@ -1,7 +1,6 @@
-/*
-    Information Theory HW4 Arimoto-Blahut Algorithm Implementation
-*/
+/* Information Theory HW4 Arimoto-Blahut Algorithm Implementation */
 #include <iostream>
+#include <iomanip>
 #include <stdio.h>
 #include <cmath>
 using namespace std;
@@ -10,86 +9,107 @@ int main() {
     // Set up the parameters
     int r = 3;
     int s = 3;
-    double p_n[r], q_n[s], I_n[r];
-    double D_n, max_I_n;
+    /*double P[r][s] = {
+        {1.0/3, 1.0/3, 1.0/6, 1.0/6},
+        {1.0/6, 1.0/3, 1.0/6, 1.0/3}
+    };*/
+    /*double P[r][s] = {
+        {1.0, 0},
+        {1.0/2, 1.0/2}
+    };*/
+    /*double P[r][s] = {
+        {1.0/2, 1.0/2, 0, 0, 0},
+        {0, 1.0/2, 1.0/2, 0, 0},
+        {0, 0, 0, 1.0/2, 1.0/2},
+        {0, 0, 0, 1.0/2, 1.0/2}
+    };*/
     double P[r][s] = {
         {0.6, 0.3, 0.1},
         {0.7, 0.1, 0.2},
         {0.5, 0.05, 0.45}
     };
-    double threshold = 0.00000000001;
+    /*double P[r][s] = {  // self-constructed transition matrix 
+        {0.1532, 0.0573, 0.2586, 0.1751, 0.3558},
+        {0.0734, 0.3836, 0.1117, 0.1930, 0.2383},
+        {0.1004, 0.1301, 0.4068, 0.1497, 0.2130},
+        {0.3057, 0.1729, 0.1173, 0.2220, 0.1821},
+        {0.1112, 0.1494, 0.1037, 0.4663, 0.1694}
+    };*/
+    double p_n[r], q_n[s], I_n[r];
+    double D_n, max_I_n;
+    double threshold = 1e-10;
     int counter = 0;
 
-    // Initialize p_n
+    // ========= Initialize p_n =========
     for (int i = 0; i < r; i++) {
-        p_n[i] = 1.0 / r;
+        p_n[i] = 1.0 / r; // uniform distribution
     }
-
-    // Arimoto-Blahut Algorithm
-    //for (int iter = 0; iter < 10; iter++) {
-    while (true) {
+    // ========= Arimoto-Blahut Algorithm Iteration =========
+    while (true) { // while true until converged
         counter++;
-        cout << "Iteration count: " << counter << endl;
-
-        // Calculate q_n
-        for (int j = 0; j < s; j++) {
+        cout << "Iteration: " << counter << endl;
+        // === Calculate q_n ===
+        for (int j = 0; j < s; j++) { // j for y and i for x
             q_n[j] = 0;
             for (int i = 0; i < r; i++) {
                 q_n[j] += p_n[i] * P[i][j];
             }
         }
-
-        // Calculate I_n
+        // === Calculate I_n ===
         for (int i = 0; i < r; i++) {
             I_n[i] = 0;
             for (int j = 0; j < s; j++) {
-                I_n[i] += P[i][j] * log(P[i][j] / q_n[j]);
+                if (P[i][j] == 0) {  // avoid calculation error
+                    I_n[i] += 0; 
+                }
+                else {
+                    I_n[i] += P[i][j] * log(P[i][j] / q_n[j]);
+                }
             }
         }
-
-        // Calculate D_n
+        // === Calculate D_n ===
         D_n = 0;
         for (int i = 0; i < r; i++) {
             D_n += p_n[i] * exp(I_n[i]);
         }
-
-        // Update p_n
+        // === Update p_n ===
         for (int i = 0; i < r; i++) {
             p_n[i] = p_n[i] * exp(I_n[i]) / D_n;
         }
 
-        for (int i = 0; i < r; i++) {
-            cout << p_n[i] << " ";
-        }
-        cout << endl;
-
-        // Check for convergence
+        // === Check for convergence === 
         max_I_n = 0;
         for (int i = 0; i < r; i++) {
             if (max_I_n < I_n[i]) {
                 max_I_n = I_n[i];
             }
         }
-        if (max_I_n - log(D_n) < threshold) {
+        if (max_I_n - log(D_n) < threshold) { // converged and break
             cout << endl << "Converged!" << endl;
             break;
         }
-        else {
-            cout << "Difference: " << max_I_n - log(D_n) << endl;
+        else { // not converged and do next iteration
+            cout << "Difference: " << max_I_n - log(D_n) << endl << endl; 
         }
     }
 
-    // Output the results
-    cout << "Iteration count: " << counter << endl;
+    // ========= Correctness check =========
+    // Following "How to Maximize a Concave Function" Corollary 2 to examine
+    cout << endl << "Correctness check (in e-base): " << endl;
     for (int i = 0; i < r; i++) {
-        cout << "p_n[" << i << "] = " << p_n[i] << endl;
+        cout << setprecision(20) << "I_n[" << i << "] = " << I_n[i] << endl;
     }
-
-    cout << endl << "Correctness check: " << endl;
+    // ========= Output the results =========
+    cout << endl << "Input distribution: " << endl;
     for (int i = 0; i < r; i++) {
-        cout << "I_n[" << i << "] = " << I_n[i] << endl;
+        cout << setprecision(20) << "p_n[" << i << "] = " << p_n[i] << endl;
     }
-
+    cout << endl << "Channel capacity (in 2-based): " << endl;
+    double I;
+    for (int i = 0; i < r; i++) {
+        I += p_n[i] * I_n[i];
+    }
+    cout << setprecision(20) << "I = " << I / log(2) << endl;
     return 0;
 }
 
